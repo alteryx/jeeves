4d23b7326b4ce81a82812ebb56eee19b4ce76734
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-01-03 02:42:15 -0800

    add stub for package level docs.

---
+++ R/jeeves-package.R
@@ -0,0 +1,10 @@
+#' Jeeves
+#'
+#' @name jeeves
+#' @docType package
+#' @importFrom grDevices colors dev.off png
+#' @importFrom stats rpois setNames
+#' @importFrom utils browseURL file.edit getFromNamespace install.packages 
+#'   installed.packages modifyList packageStatus tail unzip update.packages 
+#'   write.csv zip
+NULL
